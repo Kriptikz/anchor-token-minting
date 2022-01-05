7b69e63f16719cc273def07269fc567a418055cf
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-05 19:02:16 +0100

    Defined the Accounts needed for our CreateMint Context

--- programs/program-token-minting/src/lib.rs
+++ programs/program-token-minting/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
+use anchor_spl::token::{self, Mint};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -11,4 +13,19 @@ pub mod program_token_minting {
 }
 
 #[derive(Accounts)]
-pub struct CreateMint {}
+pub struct CreateMint<'info> {
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 6,
+        mint::authority = payer,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(signer)]
+    pub payer: AccountInfo<'info>,
+    #[account(address = system_program::ID)]
+    pub system_program: AccountInfo<'info>,
+    #[account(address = token::ID)]
+    pub token_program: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+}
