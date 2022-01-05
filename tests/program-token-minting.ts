c2b4054985af97906118cb2c607e3bae01e24b50
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-05 19:58:20 +0100

    Updated our 'Creates a Mint' test with the accounts needed to send the createMint instruction to our program Created an Airdrop SOL test so our payer can pay for the mint accounts creation Generated our Keypairs for the payer and mintA accounts created a const airdropAmount for the Airdrop Extracted provider into a const variable that we can reuse without having to grab the env() every time imported TOKEN_PROGRAM_ID from @solana/spl-token imported assert from chai

--- tests/program-token-minting.ts
+++ tests/program-token-minting.ts
@@ -1,16 +1,48 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { ProgramTokenMinting } from '../target/types/program_token_minting';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { assert } from 'chai';
 
 describe('program-token-minting', () => {
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.ProgramTokenMinting as Program<ProgramTokenMinting>;
 
-  it('Create a Mint', async () => {
-    const tx = await program.rpc.createMint({});
+  const airdropAmount = 1_000_000_000;
+
+  const payer = anchor.web3.Keypair.generate();
+  const mintA = anchor.web3.Keypair.generate();
+
+
+  it('Airdrops SOL to payer', async () => {
+    console.log("Airdropping SOL");
+
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(payer.publicKey, airdropAmount),
+      "confirmed"
+    );
+    
+    let balance = await provider.connection.getBalance(payer.publicKey);
+
+    assert.equal(airdropAmount, balance);
+
+  });
+
+  it('Creates a Mint', async () => {
+    const tx = await program.rpc.createMint({
+      accounts: {
+        mint: mintA.publicKey,
+        payer: payer.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [payer, mintA]
+    });
     console.log("Your transaction signature", tx);
   });
 });
